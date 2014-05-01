9e494ac0f2ab336b556d8de02bbf00865ed9c5b9
Joseph <joe@josephmccarron.com>
2014-05-01 10:56:47 -0700

    getting started

--- app.js
+++ app.js
@@ -2,10 +2,24 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'onAppActivated',
+
     },
 
-    doSomething: function() {
+    onAppActivated: function(data) {
+      if(data.firstLoad) {
+        //load the app, authenticate the user, and switch to the template
+          //pull credentials from localStorage
+          
+            //if no creds switch to the login template
+            var credentialsSaved = false,
+              credentialsFailed = false;
+            this.switchTo('login', {
+              saved : credentialsSaved,
+              failed: credentialsFailed
+            });
+
+      }
     }
   };
 
